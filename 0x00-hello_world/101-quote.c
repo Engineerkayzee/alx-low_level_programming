f03e60d8b8970832e1edb9f26d685890820da08a
Engineerkayzee <engineerkayzee@gmail.com>
2022-06-02 18:16:23 -0700

    a C program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include <unistd>
+
+/**
+ * main - use to retun the integer value 1
+ *
+ * Description: use the print to standard ouput in the main function
+ *
+ * Return: return the value 1
+ *
+ */
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
