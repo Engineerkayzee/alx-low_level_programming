ccf5e143437e9b1f7afc06c41216d25da24aaf25
Engineerkayzee <engineerkayzee@gmail.com>
2022-06-09 12:17:18 -0700

    fix error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 void positive_or_negative(int i);
 /**
  * main - tests function that prints if integer is positive or negative
@@ -11,5 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
